1b3349ac16e8dd3aba18ddee710588bda9f12f4d
Mikhail Deyev <mdeev@neoflex.ru>
2021-09-02 09:05:27 +0300

    small additions

--- src/main/kotlin/com/example/firstkotlinwebapp/controller/MainController.kt
+++ src/main/kotlin/com/example/firstkotlinwebapp/controller/MainController.kt
@@ -1,5 +1,6 @@
 package com.example.firstkotlinwebapp.controller
 
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -7,8 +8,10 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("/")
 class MainController {
+    val movies = mutableListOf("1st movie", "2nd movie")
+
     @GetMapping
-    fun greeting(): String {
-        return "greeting"
+    fun greeting(): ResponseEntity<MutableList<String>> {
+        return ResponseEntity.ok(movies)
     }
 }
